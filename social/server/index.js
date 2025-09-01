cc7ccea9823ab336ee977c3618659ca30cab3924
mrinal1224 <iammrinalkc@gmail.com>
2025-09-01 14:39:43 +0530

    first commit

---
+++ social/server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from './config/db.js'
+
+
+const app = express()
+const PORT = 8000 
+
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('Hello from the social server!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Social server is running on http://localhost:${PORT}`)
+})
